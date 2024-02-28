144b0c4f4ff3aa08944c8506cc9cd89adee856f2
Kéwan Bernier <mail@kewan.fr>
2024-02-28 06:27:49 +0100

    class MusicDownloader: getYtlink & downloadYT

--- funcs/downloader.js
+++ funcs/downloader.js
@@ -1,3 +1,20 @@
+import Ffmpeg from "fluent-ffmpeg";
+import ytdl from "ytdl-core";
+import SpotifyAPI from "spotify-web-api-node";
+import yts from "yt-search";
+import fs from "fs";
+
+const fsReadFile = (path) => {
+  // fs-extra readFile
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+};
+
+
 export default class MusicDownloader {
   constructor(config) {
     this.auth = {
@@ -9,5 +26,34 @@ export default class MusicDownloader {
     this.TEMP_COVERS_PATH = config.TEMP_COVERS_PATH;
     this.FINAL_PATH = config.FINAL_PATH;
   }
+
+  getYtlink = async (term) => {
+    const { videos } = await yts.search(term);
+    if (!videos || videos.length === 0) return "";
+    return videos.filter((video) => video.seconds < 3600)[0].url;
+  };
+
+  downloadYT = async (url, filename) => {
+    if (!ytdl.validateURL(url)) throw new Error("Invalid YTURL");
+    const stream = ytdl(url, {
+      quality: "highestaudio",
+      filter: "audioonly",
+    });
+    try {
+      return await new Promise((resolve, reject) => {
+        Ffmpeg(stream)
+          .audioBitrate(128)
+          .save(filename)
+          .on("error", (err) => reject(err))
+          .on("end", async () => {
+            const buffer = await fsReadFile(filename);
+            resolve(buffer);
+          });
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
 }
 

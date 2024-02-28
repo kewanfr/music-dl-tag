003f574d1d057b3227db4f3dc18135553a67477c
Kéwan Bernier <mail@kewan.fr>
2024-02-28 06:28:24 +0100

    improve lyrics finding

--- funcs/lyrics.js
+++ funcs/lyrics.js
@@ -2,18 +2,34 @@ import utils from "./utils.js"; // Import utils functions
 import { JSDOM } from "jsdom";
 import fs from "fs";
 
+function findLyricsCommentNode(dom) {
+  const iterator = dom.window.document.createNodeIterator(
+    dom.window.document.body,
+    dom.window.NodeFilter.SHOW_COMMENT,
+    null
+  );
+  let currentNode;
+  while ((currentNode = iterator.nextNode())) {
+    if (
+      currentNode.nodeValue.includes(
+        "Usage of azlyrics.com content"
+      )
+    ) {
+      return currentNode;
+    }
+  }
+  return null;
+}
+
 class LyricsFunctions {
   constructor(config) {
     this.AZ_SUGGEST_URL = "https://search.azlyrics.com/suggest.php";
-
   }
 
   async search(query) {
     let url = `${this.AZ_SUGGEST_URL}?q=${query}`;
     let response = await utils.fetchJSON(url);
-    
-    console.log(response);
-    
+
     if (response.error) {
       return response.error;
     }
@@ -26,8 +42,6 @@ class LyricsFunctions {
   }
 
   async getLyricsFromURL(url) {
-    console.log(url);
-    
     if (!url || url.length === 0) {
       throw new Error("No URL provided.");
     }
@@ -35,42 +49,37 @@ class LyricsFunctions {
     const response = await utils.fetchPage(url);
 
     const dom = new JSDOM(response);
-    const lyrics = dom.window.document.querySelector(
-      "body > div.container.main-page > div.row > div.col-xs-12.col-lg-8.text-center > div:nth-child(8)"
-    ).textContent;
+    const commentNode = findLyricsCommentNode(dom);
+    if (commentNode) {      
+      // Les paroles devraient être dans le premier élément `div` suivant le commentaire
+      const lyricsDiv =
+        commentNode.nextSibling.nodeType === 3 // Si le suivant immédiat est un noeud de texte (espace blanc probablement),
+          ? commentNode.nextSibling.nextSibling // passez au suivant
+          : commentNode.nextSibling;
 
-    if (!lyrics) {
+      if (lyricsDiv) {      
+        return lyricsDiv.parentElement.textContent.trim();
+      }
       return "No lyrics found.";
     }
-
-    // Si les lyrics sont vides
-    if (lyrics.length < 10) {
-      return "No lyrics found.";
-    }
-
-    return utils.cleanLineBreaks(lyrics);
+    return "No lyrics found.";
   }
 
-  async getLyrics(query){
+  async getLyrics(query) {
     let items = await this.search(query);
 
     if (items.length === 0) {
       return "No lyrics found.";
     }
-    console.log(items);
-    
+
     let lyrics = await this.getLyricsFromURL(items[0].url);
 
     return lyrics;
   }
 
-
   async getAndSaveTxtLyrics(query, fileName) {
-    console.log(query);
-    
-    let lyrics = await this.getLyrics(query);
-    console.log(lyrics);
-    
+    let lyrics = await this.getLyrics(query);    
+
     if (lyrics !== "No lyrics found.") {
       fs.writeFileSync(fileName, lyrics);
       return fileName;
@@ -78,8 +87,6 @@ class LyricsFunctions {
 
     return false;
   }
-
-
 }
 
 export default LyricsFunctions;

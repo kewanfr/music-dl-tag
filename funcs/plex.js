938ee0763c5850cf806108e27c441f547f6288a9
Kéwan Bernier <mail@kewan.fr>
2024-03-08 19:11:05 +0100

    feat(plex.js): plexFunctions class to get infos from Plex server Actual playing and actual lyrics

---
+++ funcs/plex.js
@@ -0,0 +1,111 @@
+import PlexAPI from "plex-api";
+
+const LYBRARY_SECTION_TITLE = "Musique";
+
+class plexFunctions {
+  constructor(config) {
+    this.plexClient = new PlexAPI({
+      hostname: config.PLEX_URL,
+      username: config.PLEX_USERNAME,
+      password: config.PLEX_PASSWORD,
+    });
+
+    this.getActualPlayingLyrics();
+  }
+
+  async getActualSessions() {
+    let sessions = await this.plexClient.find("/status/sessions");
+    return sessions;
+  }
+
+  async getActualPlaying() {
+    let sessions = await this.getActualSessions();
+    if (sessions.length === 0) {
+      return null;
+    }
+
+    let session = sessions.find(
+      (session) => session.librarySectionTitle === LYBRARY_SECTION_TITLE
+    );
+
+    if (!session) {
+      return null;
+    }
+
+    let playing = {
+      id: session.ratingKey,
+      key: session.key,
+      title: session.title,
+      type: session.type,
+      user: session.User.title,
+      artist: session.grandparentTitle,
+      album: session.parentTitle,
+      mediaPath: session.Media[0].Part[0].file,
+      player: {
+        title: session.Player.title,
+        address: session.Player.address,
+      },
+    };
+
+    return playing;
+  }
+
+  async parseLyricsResponse(response) {
+    if (response.length === 0) {
+      return null;
+    }
+    let lines = response[0].Line;
+    if (lines.length === 0) {
+      return null;
+    }
+
+    return lines.map((line) => {
+      return line.Span ? line.Span.map((span) => span.text).join(" ") : "\n";
+    }).join("\n");
+
+  }
+
+  async getMetadataFromID(id) {
+    console.log("id", id);
+
+    let metadata = await this.plexClient.query(`/library/metadata/${id}`);
+
+    const streams =
+      metadata.MediaContainer?.Metadata[0]?.Media[0]?.Part[0]?.Stream;
+
+    const lyricsStream = streams.find((stream) => stream.codec === "txt");
+
+    if (!lyricsStream) {
+      return null;
+    }
+
+    return lyricsStream;
+  }
+
+  async getLyricsFromSongID(songId) {
+    let metadata = await this.getMetadataFromID(songId);
+
+    if (!metadata) {
+      return null;
+    }
+
+    let lyrics = await this.plexClient.query(metadata.key);
+
+    if (!lyrics || !lyrics.MediaContainer?.Lyrics) {
+      return null;
+    }
+
+    return await this.parseLyricsResponse(lyrics.MediaContainer.Lyrics);  
+  }
+
+  async getActualPlayingLyrics() {
+    let playing = await this.getActualPlaying();
+    if (!playing) {
+      return null;
+    }
+
+    return await this.getLyricsFromSongID(playing.id);
+  }
+}
+
+export default plexFunctions;
